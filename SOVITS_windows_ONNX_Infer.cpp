69f2375f4995f7a4fedbf9f3b25687a79c108ea8
bigf00t <natas_hw@163.com>
2022-10-23 21:28:42 +0800

    init

---
+++ SOVITS_windows_ONNX_Infer.cpp
@@ -0,0 +1,17 @@
+﻿// SOVITS_windows_ONNX_Infer.cpp: 定义应用程序的入口点。
+//
+#define UNICODE
+#include <onnxruntime_cxx_api.h>
+#include <array>
+#include <cmath>
+#include <algorithm>
+#include "SOVITS_windows_ONNX_Infer.h"
+
+#pragma comment(lib, "onnxruntime.lib")
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
