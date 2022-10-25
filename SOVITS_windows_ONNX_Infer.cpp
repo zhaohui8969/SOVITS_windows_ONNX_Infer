6e87ebf794f22027c31607ecec76d9c805c621e3
bigf00t <natas_hw@163.com>
2022-10-26 01:31:21 +0800

    增加CUDA EP支持

--- SOVITS_windows_ONNX_Infer.cpp
+++ SOVITS_windows_ONNX_Infer.cpp
@@ -1,8 +1,10 @@
 ﻿// SOVITS_windows_ONNX_Infer.cpp: 定义应用程序的入口点。
 //
 #define UNICODE
+#define COBJMACROS
 #include "SOVITS_windows_ONNX_Infer.h"
 #include <onnxruntime_cxx_api.h>
+#include "providers.h"
 #include <array>
 #include <cmath>
 #include <algorithm>
@@ -11,9 +13,14 @@
 #include <math.h>
 #include <numeric>
 #include <chrono>
+#include <dxgi.h>
 using namespace std::chrono;
 
 #pragma comment(lib, "onnxruntime.lib")
+#pragma comment(lib, "onnxruntime_providers_cuda.lib")
+#pragma comment(lib, "onnxruntime_providers_shared.lib")
+#pragma comment(lib, "dxgi.lib")
+
 using namespace std;
 
 /*
@@ -31,12 +38,82 @@ void func_inspect_session(Ort::Session& session) {
 }
 */
 
+const OrtApi* g_ort = NULL;
+
+std::vector <IDXGIAdapter*> EnumerateAdapters(void)
+{
+	IDXGIAdapter* pAdapter;
+	std::vector <IDXGIAdapter*> vAdapters;
+	IDXGIFactory* pFactory = NULL;
+
+
+	// Create a DXGIFactory object.
+	if (FAILED(CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&pFactory)))
+	{
+		return vAdapters;
+	}
+
+
+	for (UINT i = 0;
+		pFactory->EnumAdapters(i, &pAdapter) != DXGI_ERROR_NOT_FOUND;
+		++i)
+	{
+		vAdapters.push_back(pAdapter);
+	}
+
+
+	if (pFactory)
+	{
+		pFactory->Release();
+	}
+
+	return vAdapters;
+
+}
+
+int enable_cuda(OrtSessionOptions* session_options) {
+	// OrtCUDAProviderOptions is a C struct. C programming language doesn't have constructors/destructors.
+	OrtCUDAProviderOptions o;
+	// Here we use memset to initialize every field of the above data struct to zero.
+	memset(&o, 0, sizeof(o));
+	// But is zero a valid value for every variable? Not quite. It is not guaranteed. In the other words: does every enum
+	// type contain zero? The following line can be omitted because EXHAUSTIVE is mapped to zero in onnxruntime_c_api.h.
+	o.cudnn_conv_algo_search = OrtCudnnConvAlgoSearchExhaustive;
+	o.gpu_mem_limit = SIZE_MAX;
+	OrtStatus* onnx_status = g_ort->SessionOptionsAppendExecutionProvider_CUDA(session_options, &o);
+	if (onnx_status != NULL) {
+		const char* msg = g_ort->GetErrorMessage(onnx_status);
+		LOG_INFO("启用CUDA错误:%s", msg);
+		g_ort->ReleaseStatus(onnx_status);
+		return -1;
+	}
+	return 0;
+}
+
+void func_check_dx_device() {
+	auto adapters = EnumerateAdapters();
+	LOG_INFO("DX设备数量:%d", adapters.size());
+	for (int i = 0; i < adapters.size(); i++) {
+		DXGI_ADAPTER_DESC desc;
+		adapters[i]->GetDesc(&desc);
+		//spdlog::info(desc.Description);
+		char tmp[100];
+		wcstombs(tmp, desc.Description, 100);
+		LOG_INFO("设备%d:%s", i, tmp);
+	}
+}
+
 int main()
 {
 	LOG_INFO("程序启动!");
 	LOG_INFO("读取配置文件...");
-	
+
+	func_check_dx_device();
+	int ret = 0;
+
 	std::vector<long long> fUseTimeList;
+	int iSkipWarmupStep = 5;
+	int iBenchMarkStep = 100;
 	milliseconds  tStart;
 	milliseconds tUseTime;
 	long fAvgUseTime;
@@ -46,12 +123,30 @@ int main()
 	wcscpy(sModelfile, L"hubert.onnx");
 	projectConfig.sONNXModelFile = sModelfile;
 
+	g_ort = OrtGetApiBase()->GetApi(ORT_API_VERSION);
 	Ort::Env env{ ORT_LOGGING_LEVEL_WARNING, "SOVITS_windows_ONNX_Infer" };
 	Ort::RunOptions runOptions;
-	auto allocator_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
+	OrtMemoryInfo* memory_info;
+
+	auto ortApi = OrtGetApiBase()->GetApi(ORT_API_VERSION);
+	ortApi->CreateCpuMemoryInfo(OrtArenaAllocator, OrtMemTypeDefault, &memory_info);
+	//auto allocator_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
 
 	LOG_INFO("载入HuBERT ONNX模型...");
-	Ort::Session hubertORTSession{ env, projectConfig.sONNXModelFile, Ort::SessionOptions{nullptr} };
+	Ort::SessionOptions sessionOptions;
+
+	LOG_INFO("启用CUDA...");
+	ret = enable_cuda(sessionOptions);
+	if (ret) {
+		LOG_INFO("CUD不可用");
+	}
+	else {
+		LOG_INFO("CUDA启用成功");
+	}
+	//OrtSessionOptionsAppendExecutionProvider_DML(sessionOptions, 0);
+	//sessionOptions.DisableMemPattern();
+	//sessionOptions.SetExecutionMode(ExecutionMode::ORT_SEQUENTIAL);
+	Ort::Session hubertORTSession{ env, projectConfig.sONNXModelFile, sessionOptions };
 	//func_inspect_session(hubertORTSession);
 	LOG_INFO("载入完成...");
 
@@ -68,32 +163,31 @@ int main()
 
 	std::vector<Ort::Value> huberInputList;
 	huberInputList.emplace_back(
-		Ort::Value::CreateTensor<float>(allocator_info, fTensorSource.data(), fTensorSource.size(), iTensorSourceShape.data(), iTensorSourceShape.size())
+		Ort::Value::CreateTensor<float>(memory_info, fTensorSource.data(), fTensorSource.size(), iTensorSourceShape.data(), iTensorSourceShape.size())
 	);
 
 	const char* ONNXInputNames[] = { "source" };
 	const char* ONNXoutputNames[] = { "embed" };
 
-
-
-	for (int i = 0; i < 20; i++) {
+	fUseTimeList.clear();
+	for (int i = 0; i < iBenchMarkStep; i++) {
 		tStart = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
 		std::vector<Ort::Value> returnList = hubertORTSession.Run(runOptions, ONNXInputNames, huberInputList.data(), huberInputList.size(), ONNXoutputNames, 1);
 		auto data = returnList.data();
 		auto typeAndShape = data->GetTensorTypeAndShapeInfo();
 		auto shape = typeAndShape.GetShape();
-		auto fData = data->GetTensorMutableData<float>();	
+		auto fData = data->GetTensorMutableData<float>();
 		tUseTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()) - tStart;
 		fUseTimeList.push_back(tUseTime.count());
 		LOG_INFO("ONNX 1s音频单次推理耗时:%lldms", tUseTime.count());
 	}
-	fAvgUseTime = std::accumulate(fUseTimeList.begin(), fUseTimeList.end(), 0) / fUseTimeList.size();
+	fAvgUseTime = std::accumulate(fUseTimeList.begin() + iSkipWarmupStep, fUseTimeList.end(), 0) / (fUseTimeList.size() - iSkipWarmupStep);
 	LOG_INFO("ONNX 1s音频平均耗时:%lldms", fAvgUseTime);
 
 
 	// VITS
 	LOG_INFO("载入VITS ONNX模型...");
-	Ort::Session VITSORTSession{ env, L"121_epochs.onnx", Ort::SessionOptions{nullptr} };
+	Ort::Session VITSORTSession{ env, L"121_epochs.onnx", sessionOptions };
 	LOG_INFO("载入完成...");
 
 	/*
@@ -117,22 +211,23 @@ int main()
 
 	std::vector<Ort::Value> VITSInputValues;
 	VITSInputValues.emplace_back(
-		Ort::Value::CreateTensor<float>(allocator_info, fHiddentUnit.data(), fHiddentUnit.size(), iHiddentUnitShape.data(), iHiddentUnitShape.size())
+		Ort::Value::CreateTensor<float>(memory_info, fHiddentUnit.data(), fHiddentUnit.size(), iHiddentUnitShape.data(), iHiddentUnitShape.size())
 	);
 	VITSInputValues.emplace_back(
-		Ort::Value::CreateTensor<int64_t>(allocator_info, iLength.data(), iLength.size(), iLengthShape.data(), iLengthShape.size())
+		Ort::Value::CreateTensor<int64_t>(memory_info, iLength.data(), iLength.size(), iLengthShape.data(), iLengthShape.size())
 	);
 	VITSInputValues.emplace_back(
-		Ort::Value::CreateTensor<int64_t>(allocator_info, iPitch.data(), iPitch.size(), iPitchShape.data(), iPitchShape.size())
+		Ort::Value::CreateTensor<int64_t>(memory_info, iPitch.data(), iPitch.size(), iPitchShape.data(), iPitchShape.size())
 	);
 	VITSInputValues.emplace_back(
-		Ort::Value::CreateTensor<int64_t>(allocator_info, iSid.data(), iSid.size(), iSidShape.data(), iSidShape.size())
+		Ort::Value::CreateTensor<int64_t>(memory_info, iSid.data(), iSid.size(), iSidShape.data(), iSidShape.size())
 	);
 
 	const char* VITSInputNames[] = { "hidden_unit", "lengths", "pitch", "sid" };
 	const char* VITSoutputNames[] = { "audio" };
 
-	for (int i = 0; i < 20; i++) {
+	fUseTimeList.clear();
+	for (int i = 0; i < iBenchMarkStep; i++) {
 		tStart = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
 		std::vector<Ort::Value> returnList = VITSORTSession.Run(runOptions, VITSInputNames, VITSInputValues.data(), VITSInputValues.size(), VITSoutputNames, 1);
 		auto data = returnList.data();
@@ -143,13 +238,12 @@ int main()
 		fUseTimeList.push_back(tUseTime.count());
 		LOG_INFO("VITS 1s音频单次推理耗时:%ldms", tUseTime.count());
 	}
-	fAvgUseTime = std::accumulate(fUseTimeList.begin(), fUseTimeList.end(), 0) / fUseTimeList.size();
+	fAvgUseTime = std::accumulate(fUseTimeList.begin() + iSkipWarmupStep, fUseTimeList.end(), 0) / (fUseTimeList.size() - iSkipWarmupStep);
 	LOG_INFO("VITS 1s音频平均耗时:%ldms", fAvgUseTime);
 
 	// HTTP SERVER
-	spdlog::info("启动HTTP服务");
-	spdlog::info("程序退出!");
-
+	LOG_INFO("启动HTTP服务");
+	LOG_INFO("程序退出!");
 
 	// Get F0
 
